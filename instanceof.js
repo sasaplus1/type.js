797997b1d67823ce0f27a565ee9f713f24a69062
sasaplus1 <sasaplus1@gmail.com>
2023-10-28 18:21:55 +0900

    remove instanceof.js

--- instanceof.js
+++
@@ -1,7 +0,0 @@
-const context = typeof globalThis !== 'undefined' ? globalThis : typeof self !== 'undefined' ? self : global;
-export function isMap(value) {
-    return value instanceof context.Map;
-}
-export function isSet(value) {
-    return value instanceof context.Set;
-}
