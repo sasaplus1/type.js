a90b928f025ca845ec45200f5f43d239bccec546
sasaplus1 <sasaplus1@gmail.com>
2023-10-30 13:44:12 +0900

    change to can pass context

--- instanceof.ts
+++ instanceof.ts
@@ -1,9 +1,9 @@
-const context = typeof globalThis !== 'undefined' ? globalThis : typeof self !== 'undefined' ? self : global;
+const ctx = typeof globalThis !== 'undefined' ? globalThis : typeof self !== 'undefined' ? self : global;
 
-export function isMap<K, V>(value: unknown): value is Map<K, V> {
+export function isMap<K, V>(value: unknown, context: typeof globalThis = ctx): value is Map<K, V> {
   return value instanceof context.Map;
 }
 
-export function isSet<T>(value: unknown): value is Set<T> {
+export function isSet<T>(value: unknown, context: typeof globalThis = ctx): value is Set<T> {
   return value instanceof context.Set;
 }
